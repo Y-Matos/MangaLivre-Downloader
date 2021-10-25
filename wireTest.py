bb2324bd0361ecdb27f6357fd6a0260c37ba3354
Y-Matos <yurimatos40@gmail.com>
2021-10-25 01:14:29 -0300

    Mudanças na criação das pastas e chamada da função

--- wireTest.py
+++ wireTest.py
@@ -15,27 +15,21 @@ from classeDownloader import downloadChapter
 
 total_start = time.perf_counter()
 
-#driver = WireDriver(headless=False)
 driver = criarDriver(headless=True)
+session = HTMLSession()
 
 with open('ListaCapitulos.txt','r',encoding='utf-8') as txt_capitulos:
 
-    payload = ""
-    headers = {
-        "authority": "mangalivre.net",
-        "sec-ch-ua": "^\^Chromium^^;v=^\^94^^, ^\^Google",
-        "User-Agent": "Mozilla/5.0"
-    }
-
     lista_capitulos = [(linha[:-1].split(',')) for linha in txt_capitulos]
+    nome_manga = str(lista_capitulos[1]).split('/')[4]
 
-    for capitulo in lista_capitulos[2:5]:        
+    for capitulo in lista_capitulos[0:1]:        
         chapter_start = time.perf_counter()
         link_capitulo = capitulo[1]
         num_capitulo = link_capitulo.split('/')[6]
         nome_capitulo = link_capitulo.split('/')[7]
         
-        url = f'https://mangalivre.net/ler/one-punch-man/online/{num_capitulo}/{nome_capitulo}#/!page0'
+        url = f'https://mangalivre.net/ler/one-punch-man/online/{num_capitulo}/{nome_capitulo}'
         print(f'Acessando {nome_capitulo}, {url}')
 
         driver.scopes = [f'.*https://mangalivre.net/leitor/pages/{num_capitulo}.json*']
@@ -43,18 +37,13 @@ with open('ListaCapitulos.txt','r',encoding='utf-8') as txt_capitulos:
         
 
         pages_links = json.loads(driver.last_request.response.body)
-
-        #for pagina in json_response['images']:
-        #    print(pagina['legacy'])
-        
-
         
     # Checa e cria pasta para armazenar as paginas baixadas do capitulo 
         pasta_atual = os.getcwd()
-        nova_pasta = f'{pasta_atual}\{nome_capitulo}\\'
+        nova_pasta = f'{pasta_atual}\{nome_manga}\{nome_capitulo}\\'
 
         try:
-            os.mkdir(nova_pasta)
+            os.makedirs(nova_pasta)
             print(f'Pasta {nome_capitulo} criada com sucesso. Path: {nova_pasta}')
         except OSError as error:
             print(error)
@@ -62,7 +51,7 @@ with open('ListaCapitulos.txt','r',encoding='utf-8') as txt_capitulos:
     
     # Baixa todas as imagens(paginas) do capitulo. 
         dcap = time.perf_counter()
-        session = HTMLSession()
+        
         downloadChapter(pages_links, session, nova_pasta)
         fdcap = time.perf_counter()
         print(f'Tempo baixando o capitulo: {fdcap-dcap}')
