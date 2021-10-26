d0f12c3dc5564f5235e1fd9e48cb7be86c10be72
Y-Matos <yurimatos40@gmail.com>
2021-10-25 21:43:28 -0300

    Add nova função que retorna paginas de um capitulo

---
+++ getPages.py
@@ -0,0 +1,14 @@
+import json
+
+def getChapterPages(info_capitulo, driver, nome_manga = 'null'):
+    id_capitulo = info_capitulo['Link_Capitulo'].split('/')[6]
+    num_capitulo = info_capitulo['Link_Capitulo'].split('/')[7]
+    
+    url = f'https://mangalivre.net/ler/{nome_manga}/online/{id_capitulo}/{num_capitulo}'
+
+    print(f'Acessando {num_capitulo} | {url}')
+    driver.get(url)        
+
+    json_paginas = json.loads(driver.last_request.response.body)
+    
+    return json.dumps(json_paginas,ensure_ascii= False, indent=4)
