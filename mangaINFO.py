1e0e3d659676780cb5e501337c564d33b8de18b0
Y-Matos <yurimatos40@gmail.com>
2021-10-31 21:09:27 -0300

    Add mais informações do manga

--- mangaINFO.py
+++ mangaINFO.py
@@ -2,6 +2,7 @@ import json
 import os
 from getChapters import getChaptersFastByMangaId
 from getPages import getChapterPages
+from searchManga import searchMangaByName
 from seleniumWebDriver import criarDriver
 
 class manga:
@@ -11,10 +12,17 @@ class manga:
             "manga_name" : None,
             "url_manga_name": None,
             "manga_url" : None,
+            "score": None,
+            "author": None,
+            "artist": None,
+            "cover": None,
+            "manga_url": None,
+            "categories": [],
             "chapter_count" : None,
             "chapters" : [],
         }
         self.get_manga_info(manga_id)
+        self.get_manga_searchable_info(self.info["manga_name"])
 
     def get_manga_info(self, manga_id):
         json_capitulos = json.loads(getChaptersFastByMangaId(manga_id))
@@ -24,6 +32,14 @@ class manga:
         self.info["manga_url"] = f"https://mangalivre.net/manga/{json_capitulos[0]['chapter_url'].split('/')[4]}/{manga_id}"
         self.info["chapter_count"] = len(json_capitulos)
         self.info["chapters"] = [chapter for chapter in json_capitulos]
+
+    def get_manga_searchable_info(self, manga_name):
+        returned_info = json.loads(searchMangaByName(manga_name, exact_match=True))[0]        
+        self.info["score"]= returned_info["score"]
+        self.info["author"]= returned_info["author"]
+        self.info["artist"]= returned_info["artist"]
+        self.info["cover"]= returned_info["cover"]
+        self.info["categories"]= returned_info["categories"]
 
     def get_chapter_info(self, chapter_id):
         chapter_index = next((index for (index, d) in enumerate(self.info["chapters"]) if d["chapter_id"] == chapter_id), None)
@@ -63,8 +79,7 @@ class manga:
         
         with open(f"{pasta_info}{self.info['manga_name']}_INFO.json",'w',encoding='utf-8') as json_Manga:
             json.dump(self.info, json_Manga, ensure_ascii=False, indent=4)
-            print(f"Info.json do Manga {self.info['manga_name']} salvo com sucesso.")
+            print(f"Info.json do mangá {self.info['manga_name']} salvo com sucesso.")
     
 if __name__ == '__main__':
-    mangaAleatorio = manga(1036)
-    mangaAleatorio.get_chapter_info(43700)
+    pass
