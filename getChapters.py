232eb8ae62c9322dceab4f99eeddad478b3765da
Y-Matos <yurimatos40@gmail.com>
2021-11-01 21:35:17 -0300

    add função que pega info de um capitulo especifico

--- getChapters.py
+++ getChapters.py
@@ -1,16 +1,17 @@
 from requests_html import HTMLSession
+from createFolders import criar_nova_pasta_info
+from getPages import getChapterPages
+from seleniumWebDriver import criarDriver
 import json
 
 def getChaptersFast(url):
 
     session = HTMLSession()
     source = session.get(url).content
-    source = session.get(url) #.content
-    
+    source = session.get(url)
+
     script_tag = source.html.find("[src^='/mangazord_lib/js/reader.min'] + script")[0].text.strip()
     
-
-    #script_tag = source.html.find('script')[22].text.strip()
     all_chapters_json = json.loads(script_tag[script_tag.find('['):script_tag.rfind(']')+1])
 
     lista_capitulos = []
@@ -24,9 +25,6 @@ def getChaptersFast(url):
             "chapter_index": index+1
             }
         lista_capitulos.append(info_capitulo.copy())
-
-    #with open('ListaCapitulosFast.json','w',encoding='utf-8') as json_capitulos:
-    #    json.dump(lista_capitulos[::1], json_capitulos, ensure_ascii=False, indent=4)
 
     return json.dumps(lista_capitulos[::1], ensure_ascii=False, indent=4)   # Retornar JSON OU STRING ????
 
@@ -72,6 +70,34 @@ def getChaptersFull(id_manga):
     #    json.dump(lista_capitulos[::1], json_capitulos, ensure_ascii=False, indent=4)
 
     return json.dumps(lista_capitulos[::1], ensure_ascii=False, indent=4)
+
+def get_chapter_info(chapter_id):
+
+    url = f"https://mangalivre.net/ler/null/online/{chapter_id}/capitulo-0/"
+    
+    manga_chapters = json.loads(getChaptersFast(url))
+    
+    chapter_index = next((index for (index, d) in enumerate(manga_chapters) if d["chapter_id"] == chapter_id), None)
+    chapter = manga_chapters[chapter_index]
+
+    chapter_info ={        
+        "manga_name" : chapter["chapter_url"].split('/')[4].replace("-"," ").title(),
+        "chapter_number" : chapter["chapter_number"],
+        "chapter_name" : chapter["chapter_name"],
+        "chapter_id" : chapter["chapter_id"],
+        "chapter_url" : chapter["chapter_url"],
+        "chapter_index": chapter["chapter_index"],
+        "pages": []            
+    }
+
+    chapter_info["pages"] = json.loads(getChapterPages(chapter_info, criarDriver()))
+
+    pasta_info = criar_nova_pasta_info(chapter_info["manga_name"])
+    
+    with open(f"{pasta_info}{chapter_info['manga_name']}_Capitulo_{chapter_info['chapter_number']}_INFO.json",'w',encoding='utf-8') as json_Manga:
+        json.dump(chapter_info, json_Manga, ensure_ascii=False, indent=4)
+    
+    return json.dumps(chapter_info, ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
     pass
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import json
 from time import perf_counter
 from mangaINFO import manga
+from searchManga import searchMangaByName, get_manga_id_by_name
 from downloaderFunctions import downloadManga
+from getChapters import get_chapter_info
 
 
 if __name__ == '__main__':        
     start = perf_counter()
 
-    one_punch_man = manga(1036)
-    #one_punch_man.print_info()
-    
-    one_punch_man.get_chapter_info()
+    #one_punch_man = manga(1036)
+    #one_punch_man.get_chapter_info(41252)
+    #one_punch_man.save_info()
+
+    get_chapter_info(327108)
+
     #downloadManga(one_punch_man.info, index_inicial = 1, index_final=4)
 
-    # one_piece = manga(3364)
-    # one_piece.save_info()
-
-    # opm = manga(1751)
-    # opm.save_info()
+    #print(searchMangaByName("one piece ", exact_match=True))
 
     end = perf_counter()
     total_time = end - start
